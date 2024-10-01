36b0f85b0ee53dc8ae777737b3b5c6c67dec670a
v.kydryashov <v.kudryashov@samolet.ru>
2024-10-02 01:57:37 +0300

    Happy

--- scraper.go
+++ scraper.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -20,74 +20,32 @@ func main() {
 	)
 
 	// initialize the slice of structs that will contain the scraped data
-	var products []Product
 
-	// OnHTML callback
-	c.OnHTML("body", func(e *colly.HTMLElement) {
-		// fmt.Print("Scraping: ", e)
-
-		// initialize a new Product instance
-		product := Product{}
-
-		// Find the price using a more general selector
-		e.ForEach("span", func(_ int, el *colly.HTMLElement) {
-			text := el.Text
-			if strings.Contains(text, "₽") && strings.Count(text, " ") <= 2 {
-				product.Price = strings.TrimSpace(strings.TrimSuffix(text, " ₽"))
-				return
-			}
-		})
-
-		// scrape the target data
-		product.Url = e.ChildAttr("a", "href")
-		product.Name = e.ChildText(".product-name")
-
-		// add the product instance with scraped data to the list of products
-		products = append(products, product)
+	c.OnResponse(func(r *colly.Response) {
+		err := os.WriteFile("ozon_page.html", r.Body, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("HTML content saved to ozon_page.html")
 	})
 
-	// store the data to a CSV after extraction
-	c.OnScraped(func(r *colly.Response) {
+	c.OnHTML("[data-state]", func(e *colly.HTMLElement) {
 
-		fmt.Println("output", products)
+		var product Product
 
-		return
+		dataState := e.Attr("data-state")
+		if strings.Contains(dataState, "cardPrice") {
+			var data map[string]interface{}
+			json.Unmarshal([]byte(dataState), &data)
 
-		// open the CSV file
-		file, err := os.Create("products.csv")
-		if err != nil {
-			log.Fatalln("Failed to create output CSV file", err)
-		}
-		defer file.Close()
-
-		// initialize a file writer
-		writer := csv.NewWriter(file)
-
-		// write the CSV headers
-		headers := []string{
-			"Url",
-			"Name",
-			"Price",
-		}
-
-		writer.Write(headers)
-
-		// write each product as a CSV row
-		for _, product := range products {
-			// convert a Product to an array of strings
-			record := []string{
-				product.Url,
-				product.Name,
-				product.Price,
+			if price, ok := data["cardPrice"].(string); ok {
+				product.Price = strings.TrimSpace(strings.TrimSuffix(price, " ₽"))
+				fmt.Println("Price found:", product.Price)
 			}
-
-			// add a CSV record to the output file
-			writer.Write(record)
 		}
-		defer writer.Flush()
 	})
 
 	// open the target URL
-	c.Visit("https://www.ozon.ru/product/odeyalo-ikea-stjarnbracka-teploe-150x200-sm-1648654068/?avtc=1&avte=2&avts=1727299349")
+	c.Visit("https://www.ozon.ru/product/odeyalo-ikea-stjarnbracka-teploe-150x200-sm-1648654068/")
 
 }
